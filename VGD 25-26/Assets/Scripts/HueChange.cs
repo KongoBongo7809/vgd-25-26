58b742a0131cd8c34f602820b426a13af5c46962
KongoBongo7809 <147113188+KongoBongo7809@users.noreply.github.com>
2025-09-25 20:41:27 -0400

    color change (unfinished)

---
+++ VGD 25-26/Assets/Scripts/HueChange.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class HueChange : MonoBehaviour
+{
+    [SerializeField] private Volume volume;
+    private bool isRed = true;
+    private bool isGreen = false;
+    private bool isBlue = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeRed()
+    {
+        if(!isRed)
+        {
+            
+        }
+    }
+
+    public void changeGreen()
+    {
+
+    }
+
+    public void changeBlue()
+    {
+
+    }
+}
